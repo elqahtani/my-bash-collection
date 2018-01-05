d117b94a4c6eb4b4dcb3cb8ce4cf11dcee0583d1
Huda Ridwan <kerupuk.lovers@gmail.com>
2018-01-05 14:18:09 +0700

    Modify script and add some logic with if

--- findlist.sh
+++ findlist.sh
@@ -5,9 +5,21 @@
 location=$1
 filename=$2
 
+if [ -z "$location" ] 
+then 
+	echo "Please provide location argument"
+	exit 0
+fi
+
+if [ -z "$filename" ]
+then 
+	echo "Please provide filename argument"
+	exit 0
+fi
+
 ls $location > $filename
 echo "Script is complete and has indexed the $location"
 echo "####################################"
 echo "Displaying contents of our $filename"
 echo "####################################"
-echo $filename
+cat $filename
